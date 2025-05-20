e80c663b3fe84a0bc7599965a473c6ee75e309d4
WorkSpaceForHanmg <dlzkfmtm11@naver.com>
2025-05-20 10:02:24 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'main.bundle.js'
-    },
-   module: {
-       rules: [
-         {
-           test: /\.js$/,
-           exclude: /node_modules/,
-           use: {
-             loader: 'babel-loader',
-             options: {
-               presets: ['@babel/preset-env']
-             }
-           }
-         }
-       ]
-     },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map',
-    mode: 'development'
+  //entry: './js/main.js',
+  entry: {
+    app: './js/main.js',  //app.bundle.js
+    ratefinder: './js/ratefinder.js'  //ratefinder.bundle.js
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+  mode: 'development'
 };
